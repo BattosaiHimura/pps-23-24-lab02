cf75a7d451b7d36fee1633b77b35341561685a3a
nzuluaga <nikolay.zuluaga@studio.unibo.it>
2024-03-17 22:52:44 +0100

    implemented compose function in generic mode

--- src/main/scala/task2/Compositions.scala
+++ src/main/scala/task2/Compositions.scala
@@ -1,6 +1,12 @@
 package task2
 
 object Compositions extends App :
-    def compose(f: Int => Int, g: Int => Int): Int => Int = (x: Int) => f(g(x))
 
-    println(compose(_ -1, _ * 2)(5))
+    //def compose(f: Int => Int, g: Int => Int): Int => Int = (x: Int) => f(g(x))
+    // generic type ordering is key
+    def compose[A, B, C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
+
+    def halve: Double => Double = _ / 2
+    def dec: Double => Double = _ - 1
+
+    println(compose(halve, dec)(10)) // 4,5
